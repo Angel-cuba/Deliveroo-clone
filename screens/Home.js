e561d9415fde968099127a740af15d0054f9c3a3
Mac_Developer <araozangel842@gmail.com>
2022-07-13 13:45:05 +0300

    Spliting component Home in parts

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import {
-  UserIcon,
-  ChevronDownIcon,
-  SearchIcon,
-} from 'react-native-heroicons/solid';
-import {
-  AdjustmentsIcon,
-} from 'react-native-heroicons/outline';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../parts/Categories';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -22,35 +17,12 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView className="bg-slate-50 pt-5">
-      <Text className="text-red-800">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-          <Image
-            source={{ uri: 'https://picsum.photos/id/10/200/300' }}
-            className="h-7 w-7 bg-gray-300 rounded-full"
-          />
-          <View className="flex-1">
-            <Text className="text-gray-500 font-bold text-xs">Deliver now...</Text>
-            <Text className="text-gray-700 font-bold text-xl">
-              Current location:
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
-        </View>
-
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 px-4 mx-4">
-          <View className="flex-row flex-1 space-x-2 bg-slate-200 p-3 ">
-            <SearchIcon size={20} color="gray" />
-            <TextInput placeholder="Search restaurants and cuisines" keyboardType="default" />
-          </View>
-          <AdjustmentsIcon size={20} color="#00CCBB" />
-        </View>
-
-        {/* Content */}
-        
-      </Text>
+      {/* Header */}
+      <Header />
+      {/* Search */}
+      <Search />
+      {/* Content */}
+      <Categories />
     </SafeAreaView>
   );
 }
